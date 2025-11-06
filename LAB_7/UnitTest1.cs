d89c9f3d15bfdfd2547e3a4e241250cf8c607ada
Svetlana184 <sveta.kgd.39@gmail.com>
2025-11-06 14:57:39 +0200

    Добавьте файлы проекта.

---
+++ LAB_7/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace LAB_7
+{
+    [TestFixture]
+    public class Tests
+    {
+        [TestCase]
+        public void mainTitle()
+        {
+            IWebDriver webDriver = new ChromeDriver();
+            webDriver.Url = "https://github.com/";
+            var t = webDriver.Title;
+            Assert.That(webDriver.Title, Is.EqualTo("GitHub · Change is constant. GitHub keeps you ahead. · GitHub"));
+            webDriver.Close();
+            webDriver.Quit();
+        }
+        [TestCase]
+        public void googleRequest()
+        {
+            IWebDriver webDriver = new ChromeDriver();
+            webDriver.Url = "https://www.google.com/";
+            IWebElement search = webDriver.FindElement(By.XPath("/html/body/textarea"));
+            search.SendKeys("автоматизированное тестирование");
+            webDriver.Close();
+            webDriver.Quit();
+        }
+        [TearDown]
+        public void testEnd()
+        {
+
+        }
+    }
+}
